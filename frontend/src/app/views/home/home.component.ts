2a7991f6e9ffe2ae1b2e20c8e1ec6fa5ac650829
Uri Baynesay <uri552@gmail.com>
2024-12-11 18:09:14 +0200

    added github and project link

--- frontend/src/app/views/home/home.component.ts
+++ frontend/src/app/views/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ProjectListComponent } from '../../components/project-list/project-list.component';
 import { HeroComponent } from '../../components/hero/hero.component';
+import { MatProgressSpinner } from "@angular/material/progress-spinner";
 
 @Component({
   selector: 'app-home',
-  imports: [ProjectListComponent, HeroComponent],
+  imports: [ProjectListComponent, HeroComponent, MatProgressSpinner],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
