de1545997b6ea10604978608a168e5430ab31e6f
Uri Baynesay <uri552@gmail.com>
2024-12-15 16:38:02 +0200

    merge to production

--- frontend/src/app/service/project/project.service.ts
+++ frontend/src/app/service/project/project.service.ts
@@ -8,9 +8,9 @@ import { Project } from '../../interfaces/project';
 export class ProjectService {
   http = inject(HttpClient);
   getProjects = () => {
-    return this.http.get<Project[]>('/api/project');
+    return this.http.get<Project[]>('http://localhost:3000//api/project');
   };
   getProjectById = (id: string) => {
-    return this.http.get<Project>('/api/project/' + id);
+    return this.http.get<Project>('http://localhost:3000//api/project/' + id);
   };
 }
