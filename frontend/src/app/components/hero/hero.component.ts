5f6cc5e569f68aef3b92643aa751c732b7d1aed5
Uri Baynesay <uri552@gmail.com>
2024-12-24 15:57:41 +0200

    homepage styling

--- frontend/src/app/components/hero/hero.component.ts
+++ frontend/src/app/components/hero/hero.component.ts
@@ -1,11 +1,19 @@
+import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-hero',
-  imports: [],
+  imports: [NgOptimizedImage],
   templateUrl: './hero.component.html',
-  styleUrl: './hero.component.scss'
+  styleUrl: './hero.component.scss',
 })
 export class HeroComponent {
-
+  stack = [
+    { name: 'NextJS', color: 'black' },
+    { name: 'React', color: 'blue-500' },
+    { name: 'Node', color: 'green-700' },
+    { name: 'ExpressJS', color: 'black' },
+    { name: 'MongoDB', color: 'green-500' },
+    { name: 'PostgreSQL', color: 'blue-700' },
+  ];
 }
